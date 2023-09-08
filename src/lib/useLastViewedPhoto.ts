9ff41be7e429ca9af620d57bd5ac74871892e2bf
kelvinsekx <utdkelvin@gmail.com>
2023-09-08 09:12:36 +0000

    update globalState types

--- src/lib/useLastViewedPhoto.ts
+++ src/lib/useLastViewedPhoto.ts
@@ -1,7 +1,8 @@
 import { createGlobalState } from 'react-hooks-global-state'
 
+type TInitState = {photoToScrollTo: null | number}
 const initialState = { photoToScrollTo: null }
-const { useGlobalState } = createGlobalState(initialState)
+const { useGlobalState } = createGlobalState<TInitState>(initialState)
 
 export const useLastViewedPhoto = () => {
   return useGlobalState('photoToScrollTo')
