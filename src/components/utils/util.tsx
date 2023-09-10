2aa78da1ee0182997c5c222f357b2a45786ca833
kelvinsekx <utdkelvin@gmail.com>
2023-09-10 11:52:25 +0000

    ADS: 02

--- src/components/utils/util.tsx
+++ src/components/utils/util.tsx
@@ -35,5 +35,10 @@ export function convertDateString(dateString: string): string[] {
 }
 
 export const getHumaneDate = (date: string)=> {
-  return convertDateString(date).toString().replaceAll(',', ', ')
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit'
+  }
+  return new Date(date).toLocaleDateString(undefined, options)
 }
