8c5074d13ee891423db50d9edb57343fcf6f7a6e
kelvinsekx <utdkelvin@gmail.com>
2023-09-10 07:54:56 +0000

    push branch and close

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,23 @@
 import MainBanner from '@/components/Layouts/banner'
 import WelcomeToChurch from "@/components/Layouts/welcomeToChurch";
-// import Posters from "@/components/Layouts/posters";
-// import WorshipLive from "@/components/worshipLive";
-// import { Gallery } from "@/components/Gallery/gallery";
-//  import { Services } from "@/components/Services/service";
-//  import LatestSermo from "@/components/Sermons/latestSermon";
-// import { LocateUs } from "@/components/LocateUs/locateus";
+import Posters from "@/components/Layouts/posters";
+import WorshipLive from "@/components/worshipLive";
+import { Gallery } from "@/components/Gallery/gallery";
+ import { Services } from "@/components/Services/service";
+ import LatestSermo from "@/components/Sermons/latestSermon";
+import { LocateUs } from "@/components/LocateUs/locateus";
 
 export default async function Home() {
   return (
     <div>
       <MainBanner />
       <WelcomeToChurch />
-      {/* <Posters />
+      <Posters />
       <WorshipLive /> 
       <Gallery /> 
       <Services /> 
       <LatestSermo />
-      <LocateUs /> */}
+      <LocateUs />
     </div>
   );
 }
