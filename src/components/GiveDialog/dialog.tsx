366046541b27f6a2e2d0dbd89d4db607f9cc51d6
kelvinsekx <utdkelvin@gmail.com>
2023-09-10 02:43:16 +0000

    responsize Give Dialog

--- src/components/GiveDialog/dialog.tsx
+++ src/components/GiveDialog/dialog.tsx
@@ -14,13 +14,15 @@ import { CheckIcon } from "lucide-react"
 import React from "react"
 import { Description } from "../Description"
 
+import Image from 'next/image'
+
 export function GiveDialog() {
   return (
     <Dialog>
       <DialogTrigger asChild>
         <header className="text-white px-2 h-full bg-tb-green flex items-center">GIVE</header>
       </DialogTrigger>
-      <DialogContent className="w-fit">
+      <DialogContent className="">
         <DialogHeader>
           <DialogTitle className="text-tb-black text-3xl md:text-4xl py-2 self-center">Make a Donation</DialogTitle>
         </DialogHeader>
@@ -42,29 +44,14 @@ export function GiveDialog() {
           <div>
             <div className="pb-4">Select payment method:</div>
             <div className="flex flex-col md:flex-row gap-4">
-            <div className="flex w-36 h-10">
-                <div className="w-full h-full flex items-center justify-center border-2 hover:border-tb-green">
-                  <span>Cashapp</span>
-                </div>
-                <span className="h-4 w-4 border-2 bg-tb-green rounded-full flex items-center justify-center relative bottom-2"><CheckIcon className="text-white w-4 h-4" /></span>
-            </div>
-            <div className="flex w-36 h-10">
-                <div className="w-full h-full flex items-center justify-center border-2 hover:border-tb-green">
-                  <span>Zelle</span>
-                </div>
-                <span className="h-4 w-4 border-2 bg-tb-green rounded-full flex items-center justify-center relative bottom-2"><CheckIcon className="text-white w-4 h-4" /></span>
-            </div>
-            <div className="flex w-36 h-10">
-                <div className="w-full h-full flex items-center justify-center border-2 hover:border-tb-green">
-                  <span>Credit Card</span>
-                </div>
-                <span className="h-4 w-4 border-2 bg-tb-green rounded-full flex items-center justify-center relative bottom-2"><CheckIcon className="text-white w-4 h-4" /></span>
-            </div>
+              <AccountCard name='Cashapp' src='/cashapp-logo.png'></AccountCard>
+              <AccountCard name='Zelle'></AccountCard>
+              <AccountCard name='Credit Card'></AccountCard>
             </div>
             <div className="py-4">
               <p className="font-bold text-tb-grey2">Cashapp</p>
               <p>You can make payments to the account info below</p>
-              <div className="text-tb-grey2 font-bold">9196413832 -$Jesustabernacle2</div>
+              <div className="text-tb-grey2 font-bold text-lg">9196413832 -$Jesustabernacle2</div>
             </div>
           </div>
         </div>
@@ -82,3 +69,15 @@ const GLabel: React.FC<React.ComponentPropsWithoutRef<'div'>> = ({children})=><d
 </div>
 
 GiveDialog.Label = GLabel
+
+const AccountCard: React.FC<React.ComponentPropsWithoutRef<'div'> & {name: string, src?: string}> = ({name, src='/zelle-logo.png'})=>  <div className="flex">
+  <div className="flex w-52 md:w-36 border-2 hover:border-tb-green p-2">
+    <div className="relative h-8 w-12"> <Image src={src} fill alt='zelle logo' /></div>
+    <div className="w-full h-full flex items-center justify-center">
+      <span>{name}</span>
+    </div>
+  </div>
+  <span className="h-4 w-4 border-2 bg-tb-green rounded-full flex items-center justify-center relative bottom-2"><CheckIcon className="text-white w-4 h-4" /></span>
+</div>
+
+GiveDialog.AccountCard = AccountCard
