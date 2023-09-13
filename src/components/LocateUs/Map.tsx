6dcdf0c7567067b2d1c3a56a3887179eb4c6310d
kelvinsekx <utdkelvin@gmail.com>
2023-09-13 14:01:00 -0700

    WIP 2

--- src/components/LocateUs/Map.tsx
+++ src/components/LocateUs/Map.tsx
@@ -8,7 +8,7 @@ export const Map = () => {
         googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!
     })
 
-    const center = useMemo(()=>({lat:36.014807, lng:-78.933660}))
+    const center = useMemo(()=>({lat:36.014807, lng:-78.933660}), [])
     if(!isLoaded) return <div>loading...</div>
     return <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
         <Marker position={center}/>
