e448ad677f9bad3e06127157e2e8937658958e89
kelvinsekx <utdkelvin@gmail.com>
2023-09-14 10:40:14 +0000

    WIP: 3

--- src/components/Layouts/banner.tsx
+++ src/components/Layouts/banner.tsx
@@ -14,25 +14,64 @@ const sources = [
 const Banner = ()=> {
     return <div className="_34sdf max-w-[100rem]">
         <Banner.Carousel
-          items={sources.map((src, index) => (
-            <Banner.Card src={src} key={index} />
-          ))}
+          items={[
+            <Card />,
+            <Card2 />,
+            <Card3 />
+          ]}
         />
       </div>
 }
 
-const Card = ({src}: {src: string})=> <div><div className='relative overflow-hidden h-[22rem] lg:h-[35rem] w-full'>
-   <Image src={src} alt='' fill className='object-cover aspect-[3/4]' />
-</div>
-<p className='text-white absolute top-[20%] md:top-[5%] xl:top-[20%] left-1/2 -translate-x-1/2 w-full md:w-[80%] text-center flex flex-col items-center gap-4'>
-  <span className='text-4xl  md:text-7xl lg:text-8xl'>SUNDAY WORSHIP SERVICE</span> <span className='text-2xl border-4 border-white p-4'>{`8am - 12pm`.toUpperCase()}</span>
-  </p>
-</div>
-Banner.Card = Card
 
 export const Carousel = ({ items }: { items: React.ReactNode[] }) => (
   <AliceCarousel animationDuration={3000} autoPlayInterval={4000} autoHeight autoPlay={true} infinite mouseTracking items={items} />
 )
 Banner.Carousel = Carousel
+
+const CardFoot: React.FC<React.ComponentPropsWithoutRef<'span'>> = ({children})=> <span className='text-xl md:text-2xl border-4 border-white p-2 md:p-4 rounded'>{children}</span>
+const CardContent: React.FC<React.ComponentPropsWithoutRef<'div'>> = ({children})=> <div className='overflow-hidden font-mono'>{children}</div>
+
+
+
+const Card = ()=> <CardContent>
+  <div className='relative overflow-hidden h-[22rem] lg:h-[35rem] w-full'>
+   <Image src={sources[0]} alt='' fill className='object-cover aspect-[3/4]' />
+  </div>
+  <div className='text-white absolute top-[20%] md:top-[5%] xl:top-[20%] left-1/2 -translate-x-1/2 w-full md:w-[80%] text-center gap-4 '>
+    <div className='flex flex-col items-center'>
+      <span className='text-4xl  md:text-7xl lg:text-8xl'>SUNDAY WORSHIP SERVICE</span> 
+      <CardFoot>{`9am - 12pm`.toUpperCase()}</CardFoot>
+    </div>
+  </div>
+</CardContent>
+Banner.Card = Card
+
+const Card2 = ()=> <CardContent>
+  <div className='relative overflow-hidden h-[22rem] lg:h-[35rem] w-full'>
+   <Image src={sources[1]} alt='' fill className='object-cover aspect-[3/4]' />
+  </div>
+  <div className='text-white absolute top-[20%] md:top-[5%] xl:top-[20%] left-1/2 -translate-x-1/2 w-full md:w-[80%] text-center gap-4 '>
+    <div className='flex flex-col items-center'>
+      <span className='text-4xl  md:text-7xl lg:text-8xl'>It’s a new dawn</span> 
+      <CardFoot>{`Come and walk the path of grace with us!`}</CardFoot>
+    </div>
+  </div>
+</CardContent>
+Banner.Card2 = Card2
+
+const Card3 = ()=> <CardContent>
+  <div className='relative overflow-hidden h-[22rem] lg:h-[35rem] w-full'>
+   <Image src={sources[2]} alt='' fill className='object-cover aspect-[3/4]' />
+  </div>
+  <div className='text-white absolute top-[10%] md:top-[5%] xl:top-[20%] left-1/2 md:left-[30%] -translate-x-1/2 w-full md:w-[80%] text-center gap-4 '>
+    <div className='flex flex-col items-center'>
+      <span className='text-4xl  md:text-7xl lg:text-8xl'>Bible study</span> 
+      <CardFoot>{`Come and walk the path of grace with us!`}</CardFoot>
+    </div>
+  </div>
+</CardContent>
+Banner.Card3 = Card3
+
 
 export default Banner
--- src/components/Services/service.tsx
+++ src/components/Services/service.tsx
@@ -15,27 +15,27 @@ const events = [
     time: '9am - 10pm (prayer line)'
   },
   {
-    day: 'Monday',
+    day: 'TUESDAY',
+    title: 'Noon Prayers',
+    time: '9am - 10pm (prayer line)'
+  },
+  {
+    day: 'WEDNESDAY',
     title: 'Fresh Annointing',
     time: '9am - 10pm (prayer line)'
   },
   {
-    day: 'Monday',
+    day: 'THURSDAY',
     title: 'Fresh Annointing',
     time: '9am - 10pm (prayer line)'
   },
   {
-    day: 'Monday',
+    day: 'FRIDAY',
     title: 'Fresh Annointing',
     time: '9am - 10pm (prayer line)'
   },
   {
-    day: 'Monday',
-    title: 'Fresh Annointing',
-    time: '9am - 10pm (prayer line)'
-  },
-  {
-    day: 'Monday',
+    day: 'SUNDAY',
     title: 'Fresh Annointing',
     time: '9am - 10pm (prayer line)'
   },
