f8336a3e066a8c99b64fcc840771d419059ec014
kelvinsekx <utdkelvin@gmail.com>
2023-09-14 06:49:13 +0000

    WIP: 1

--- src/components/Layouts/banner.tsx
+++ src/components/Layouts/banner.tsx
@@ -6,11 +6,9 @@ import AliceCarousel from 'react-alice-carousel'
 import 'react-alice-carousel/lib/alice-carousel.css'
 
 const sources = [
-  "https://res.cloudinary.com/dfliypjzt/image/upload/q_50,f_auto,fl_lossy,w_auto,c_scale/v1686718456/a_speaker_at_the_ibadan_techies_kff5so",
-  "https://res.cloudinary.com/dfliypjzt/image/upload/q_50,f_auto,fl_lossy,w_auto,c_scale/v1686712604/techies_at_a_meetup_in_Nigeria_pnjhwb",
-  "https://res.cloudinary.com/dfliypjzt/image/upload/q_50,f_auto,fl_lossy,w_auto,c_scale/v1686713329/happy_techies_at_a_tech_meetup_in_nigeria_guvvww",
-  "https://res.cloudinary.com/dfliypjzt/image/upload/q_50,f_auto,fl_lossy,w_auto,c_scale/v1686713737/two_attendees_at_ibadan_techies_meetup_mgtzlr",
-  "https://res.cloudinary.com/dfliypjzt/image/upload/q_50,f_auto,fl_lossy,w_auto,c_scale/v1681138175/gallery/grid1_2d36_eqhbcn",
+  "https://res.cloudinary.com/joi-de-vivro/image/upload/q_50,f_auto,fl_lossy,w_auto,c_scale/v1694668748/others/Banner2_1_ulsqzx.jpg",
+  "https://res.cloudinary.com/joi-de-vivro/image/upload/q_50,f_auto,fl_lossy,w_auto,c_scale/v1694668753/others/Banner3_1_rm1cpk.jpg",
+  "https://res.cloudinary.com/joi-de-vivro/image/upload/q_50,f_auto,fl_lossy,w_auto,c_scale/v1694668743/others/Banner1_1_wx6ons.jpg"
 ];
 
 const Banner = ()=> {
@@ -23,13 +21,17 @@ const Banner = ()=> {
       </div>
 }
 
-const Card = ({src}: {src: string})=> <div className='relative h-[22rem] lg:h-[35rem] w-full'>
+const Card = ({src}: {src: string})=> <div><div className='relative overflow-hidden h-[22rem] lg:h-[35rem] w-full'>
    <Image src={src} alt='' fill className='object-cover aspect-[3/4]' />
+</div>
+<p className='text-white absolute top-[20%] md:top-[5%] xl:top-[20%] left-1/2 -translate-x-1/2 w-full md:w-[80%] text-center flex flex-col items-center gap-4'>
+  <span className='text-4xl  md:text-7xl lg:text-8xl'>SUNDAY WORSHIP SERVICE</span> <span className='text-2xl border-4 border-white p-4'>{`8am - 12pm`.toUpperCase()}</span>
+  </p>
 </div>
 Banner.Card = Card
 
 export const Carousel = ({ items }: { items: React.ReactNode[] }) => (
-  <AliceCarousel animationDuration={2500} autoPlayInterval={3000} autoHeight autoPlay={true} infinite mouseTracking items={items} />
+  <AliceCarousel animationDuration={3000} autoPlayInterval={4000} autoHeight autoPlay={true} infinite mouseTracking items={items} />
 )
 Banner.Carousel = Carousel
 
